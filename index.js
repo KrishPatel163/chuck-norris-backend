c158d8c047569588537c61693f40e5476584c7b0
KrishPatel163 <krishhpatel7587@gmail.com>
2024-05-11 18:51:01 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/api/jokes', async (req, res) => {
+    try {
+        let apiUrl = "https://api.chucknorris.io/jokes/random";
+
+        // Check if category query parameter is provided
+        let categoryResponse = await axios.get("https://api.chucknorris.io/jokes/categories");
+        if (req.query.category) {
+            apiUrl += `?category=${req.query.category}`;
+        } else {
+            const randomCategory = categoryResponse.data[Math.floor(Math.random() * categoryResponse.data.length)];
+            apiUrl += `?category=${randomCategory}`;
+        }
+
+        // Fetch joke from Chuck Norris API
+        const jokesResponse = await axios.get(apiUrl);
+
+        // Send response with joke data
+        res.send({
+            joke: jokesResponse.data,
+            categories: categoryResponse.data
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ "message": "Something went wrong while getting jokes" });
+    }
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}. Check at: http://localhost:${PORT}`);
+});
